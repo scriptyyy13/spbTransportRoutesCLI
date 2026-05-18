cc3415a17c4bb055993fc2948d55908b6c60484d
Scriptyyy <scriptyyy@mail.ru>
2026-05-18 13:33:27 +0300

    feat: список команд вложен для читаемости, как и в других командах

--- src/main/kotlin/command/HelpCommand.kt
+++ src/main/kotlin/command/HelpCommand.kt
@@ -5,7 +5,7 @@ class HelpCommand(private val commands: List<Command>) : Command {
     override fun execute(args: List<String>) {
         println("Список команд:")
         for (cmd in commands) {
-            println("${cmd.getName()} - ${cmd.getHelp()}")
+            println("   ${cmd.getName()} - ${cmd.getHelp()}")
         }
     }
 
