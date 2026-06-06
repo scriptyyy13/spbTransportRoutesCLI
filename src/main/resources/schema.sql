891f44b0b411ffb6c47cccd377938dd077a16844
Scriptyyy <scriptyyy@mail.ru>
2026-06-07 02:07:51 +0300

    fix: дропаем индексы если они уже есть и пересоздаем

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,10 @@
+DROP INDEX IF EXISTS idx_route_stops_route_and_order;
 CREATE INDEX idx_route_stops_route_and_order ON route_stops (route_id, stop_number);
+DROP INDEX IF EXISTS idx_routes_name_exact;
 CREATE INDEX idx_routes_name_exact ON routes (route_long_name);
+DROP INDEX IF EXISTS idx_stops_name_exact;
 CREATE INDEX idx_stops_name_exact ON stops (name);
+DROP INDEX IF EXISTS idx_routes_route_id;
 CREATE INDEX idx_routes_route_id ON routes (route_id);
+DROP INDEX IF EXISTS idx_stops_stop_id;
 CREATE INDEX idx_stops_stop_id ON stops (stop_id);
