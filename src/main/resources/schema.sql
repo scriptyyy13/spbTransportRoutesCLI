ec0de3342986f0d7ff18f7e5df520e38d24e0273
Scriptyyy <scriptyyy@mail.ru>
2026-06-07 02:03:25 +0300

    feat: индексы на id маршрутов и остановок

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,5 @@
-CREATE INDEX IF NOT EXISTS idx_route_stops_route_and_order ON route_stops (route_id, stop_number);
-CREATE INDEX IF NOT EXISTS idx_routes_name_exact ON routes (route_long_name);
-CREATE INDEX IF NOT EXISTS idx_stops_name_exact ON stops (name);
+CREATE INDEX idx_route_stops_route_and_order ON route_stops (route_id, stop_number);
+CREATE INDEX idx_routes_name_exact ON routes (route_long_name);
+CREATE INDEX idx_stops_name_exact ON stops (name);
+CREATE INDEX idx_routes_route_id ON routes (route_id);
+CREATE INDEX idx_stops_stop_id ON stops (stop_id);
